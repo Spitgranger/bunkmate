d109aeb73efa3e2b4e0c169dbd22ef2fd962da19
spitgranger <richard.fan2000@hotmail.com>
2023-01-20 16:06:38 -0500

    initial commit of backend boilerplate

---
+++ server/controllers/posts.js
@@ -0,0 +1,7 @@
+export const getPosts = (req, res) => {
+    res.json({
+        data: "laoshu",
+        name: "post",
+        date: "2023-01-20",
+    })
+};
--- server/server.js
+++ server/server.js
@@ -2,17 +2,18 @@ import express from 'express';
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
-
+import postRoutes from './routes/posts.js'
 
 const app = express();
 dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use('/', postRoutes);
 
 
 const PORT = process.env.PORT || 5000;
-const CONNECTION_URL = "mongodb://127.0.0.1/memories" || process.env.CONNECTION_URL;
+const CONNECTION_URL = "mongodb://127.0.0.1/bunkmate" || process.env.CONNECTION_URL;
 
 mongoose.set('strictQuery', false)
 mongoose.connect(CONNECTION_URL, {
