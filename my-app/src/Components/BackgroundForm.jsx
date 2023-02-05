2b64fba2af5d088e6e8265c9669b32a8a16f16c7
zhwang2001 <zhwang2001@gmail.com>
2023-02-04 23:59:25 -0500

    reduced repetition, input validation and regex expression for socials link

--- my-app/src/Components/BackgroundForm.jsx
+++ my-app/src/Components/BackgroundForm.jsx
@@ -192,6 +192,7 @@ function Background({ forwardButton }) {
   const handleEmailValidation = (e) => {
     const email = e.target.value
     const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
     if (!emailRegex.test(email)) {
       setEmailError(true);
       setEmailHelperText('Please enter a valid email address')
@@ -201,7 +202,24 @@ function Background({ forwardButton }) {
     }
   };
 
-  //Multilinetext verification
+  //Social media link valdiation
+  //must follow a specific syntax
+  const [link, setLink] = useState(false);
+  const [LinkHelperText, setLinkHelperText] = useState('Optional');
+
+  const handleLinkValidation = (e) => {
+    const link = e.target.value
+    const linkRegex = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/
+    if (!linkRegex.test(link)) {
+      setLink(true);
+      setLinkHelperText('Please enter a valid url')
+    } else {
+      setLink(false);
+      setLinkHelperText('')
+    }
+  };
+
+  //Multilinetext validation
   //can't exceed 500 characters
   const [textHelperText, setTextHelperText] = useState("Max: 500 Characters");
   const [textError, setTextError] = useState(false);
@@ -232,35 +250,9 @@ function Background({ forwardButton }) {
 
   }
 
-  const handleGenderChange = e => {
-    setValues(prevValue => ({ ...prevValue, gender: e.target.value }));
-  }
-  const handleNameChange = e => {
-    setValues(prevValue => ({ ...prevValue, firstName: e.target.value }));
-  }
-  const handleEducationChange = e => {
-    setValues(prevValue => ({ ...prevValue, education: e.target.value }));
-  }
-  const handleEmploymentChange = e => {
-    setValues(prevValue => ({ ...prevValue, employment: e.target.value }));
-  }
-  const handleIncomeChange = e => {
-    setValues(prevValue => ({ ...prevValue, income: e.target.value }));
-  }
-  const handleCreditChange = (e) => {
-    setValues(prevValue => ({ ...prevValue, credit: e.target.value }));
-  }
-  const handleAddressChange = (e) => {
-    setValues(prevValue => ({ ...prevValue, address: e.target.value }));
-  }
-  const handleEmailChange = (e) => {
-    setValues(prevValue => ({ ...prevValue, email: e.target.value }));
-  }
-  const handleLastNameChange = (e) => {
-    setValues(prevValue => ({ ...prevValue, lastName: e.target.value }));
-  }
-  const handlePhoneChange = (e) => {
-    setValues(prevValue => ({ ...prevValue, phone: e.target.value }));
+  const handleFieldChange = (e, field) => {
+    setValues(prevValue => ({ ...prevValue, [field]: e.target.value }));
+
   }
 
   //memoize inputs to save rerendering all components on one change.
@@ -274,29 +266,29 @@ function Background({ forwardButton }) {
     <UploadFile helperText="Supported Files: jpg, png, " helperTextPos="45%" width="50%" type="file" message="Upload Profile Picture" accept={["image/jpg", "image/jpeg", "image/png"]} endIcon={<CameraAltIcon sx={{ color: "aqua" }} />} handleFileUpload={handleFileUpload} />
     {values.picture ? <img src={values.picture} style={{ width: "30%", height: "40%", borderRadius: "5px" }}></img> : null}
     <LineBox flex={true} CssTextField={[
-      <FormSingleLineInput size='small' type="text" field="Legal First Name" placeHolder="Sam" onChange={handleNameChange} value={values.firstName} />,
-      <FormSingleLineInput size="small" type="text" field="Legal Last Name" placeHolder="Jenkins" onChange={handleLastNameChange} value={values.lastName} />,]
+      <FormSingleLineInput size='small' type="text" field="Legal First Name" placeHolder="Sam" onChange={(e) => { handleFieldChange(e, 'firstName') }} value={values.firstName} />,
+      <FormSingleLineInput size="small" type="text" field="Legal Last Name" placeHolder="Jenkins" onChange={(e) => { handleFieldChange(e, 'lastName') }} value={values.lastName} />,]
     } />
     <div id="multiline">
       <FormMultiLineInput placeHolder="Tell us a bit about yourself" type="text" field="About Me" helperText={textHelperText} onChange={handleTextField} error={textError} value={values.about} />
     </div>
 
     <LineBox flex={true} CssTextField={[
-      <DropDownMenu label="Gender" menuItem={["Male", "Female", "Other"]} value={values.gender} onChange={handleGenderChange} />,
-      <FormSingleLineInput size="small" type="text" field="Linkedin Profile" placeHolder="(Optional)" />
+      <DropDownMenu label="Gender" menuItem={["Male", "Female", "Other"]} value={values.gender} onChange={(e) => { handleFieldChange(e, 'gender') }} />,
+      <FormSingleLineInput error={link} helperText={LinkHelperText} onChange={(e) => { handleFieldChange(e, 'links'); handleLinkValidation(e) }} size="small" type="text" field="Social Media Profile" placeHolder="ex. https://www.linktr.ee/john_smith" value={values.links} />
     ]
     } />
     <br></br>
 
     <FormSection title="Personal Info" message="*We collect this data for our algorithms, we won't share it with anyone else. We'll ask you for proof on the next page" />
     <LineBox flex={true} CssTextField={[
-      <FormSingleLineInput size="small" type="text" field="Email" placeHolder="ex. bunkmates@gmail.com" error={emailError} helperText={emailHelperText} value={values.email} onChange={(e) => { handleEmailChange(e); handleEmailValidation(e); }} />,
+      <FormSingleLineInput size="small" type="text" field="Email" placeHolder="ex. bunkmates@gmail.com" error={emailError} helperText={emailHelperText} value={values.email} onChange={(e) => { handleFieldChange(e, 'email'); handleEmailValidation(e); }} />,
       <DatePicker label="Birthday" onChange={handleChange} value={value} />
     ]
     } />
     <LineBox flex={true} CssTextField={[
-      <FormSingleLineInput type="text" size="small" helperText={phoneHelperText} field="Phone Number" placeHolder="6472345124" error={phoneError} onChange={(e) => { handlePhoneChange(e); handlePhoneValidation(e); }} value={values.phone} />,
-      <FormSingleLineAddressInput type="text" field="Address" placeHolder="31 West Street" inputRef={inputRef} value={values.address} onChange={handleAddressChange} />
+      <FormSingleLineInput type="text" size="small" helperText={phoneHelperText} field="Phone Number" placeHolder="6472345124" error={phoneError} onChange={(e) => { handleFieldChange(e, 'phone'); handlePhoneValidation(e); }} value={values.phone} />,
+      <FormSingleLineAddressInput type="text" field="Address" placeHolder="31 West Street" inputRef={inputRef} value={values.address} onChange={(e) => { handleFieldChange(e, 'address') }} />
     ]
     } />
     {values.city && values.country && values.province ?
@@ -307,16 +299,16 @@ function Background({ forwardButton }) {
       ]
       } /> : null}
     <LineBox flex={true} CssTextField={[
-      <DropDownMenu label="Employment" menuItem={["Current Employed", "Currently Unemployed", "Currently Self Employed"]} value={values.employment} onChange={handleEmploymentChange} />,
-      <DropDownMenu label="Current Education" menuItem={["Not in School", "High School", "Undergraduate Studies", "Graduate Studies"]} value={values.education} onChange={handleEducationChange} />,
+      <DropDownMenu label="Employment" menuItem={["Current Employed", "Currently Unemployed", "Currently Self Employed"]} value={values.employment} onChange={(e) => { handleFieldChange(e, "employment") }} />,
+      <DropDownMenu label="Current Education" menuItem={["Not in School", "High School", "Undergraduate Studies", "Graduate Studies"]} value={values.education} onChange={(e) => { handleFieldChange(e, 'education') }} />,
     ]
     } />
     <br></br>
     <FormSection title="Finances and Verification" message="*You can provide us proof later" />
     {/* ranges from 10000 - 100000*/}
     <LineBox flex={true} CssTextField={[
-      <FormSingleLineInput size="small" helperText={creditHelperText} error={creditError} field="Credit Score" placeHolder="ex. 740" value={values.credit} onChange={(e) => { handleCreditChange(e); handleCreditValidation(e) }} />,
-      <DropDownMenu label="Annual Income" menuItem={["< $10000", "$10000 - $50000", "$50001 - $100000", "$100001 - $200000", "> $200001"]} value={values.income} onChange={handleIncomeChange} />,
+      <FormSingleLineInput size="small" helperText={creditHelperText} error={creditError} field="Credit Score" placeHolder="ex. 740" value={values.credit} onChange={(e) => { handleFieldChange(e, 'credit'); handleCreditValidation(e); }} />,
+      <DropDownMenu label="Annual Income" menuItem={["< $10000", "$10000 - $50000", "$50001 - $100000", "$100001 - $200000", "> $200001"]} value={values.income} onChange={(e) => { handleFieldChange(e, 'income') }} />,
     ]
     } />
 
