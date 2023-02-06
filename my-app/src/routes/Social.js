54333ffc9a3c6adaab58f34fbb4bc3cd7fb4f3c2
spitgranger <richard.fan2000@hotmail.com>
2023-02-06 12:35:53 -0500

    updated the ways that profile cards are displated

--- my-app/src/routes/Social.js
+++ my-app/src/routes/Social.js
@@ -1,6 +1,6 @@
 import react, { useRef, useState } from "react";
 import Navbar from "../Components/Navbar";
-import { GoogleMap, useJsApiLoader, MarkerF } from "@react-google-maps/api";
+import { GoogleMap, useJsApiLoader, MarkerF, OverlayView, OVERLAY_MOUSE_TARGET, OVERLAY_LAYER } from "@react-google-maps/api";
 import mapStyles from './mapStyles.json'
 import { Button, Grid, Paper, TextField, Card, Typography, CardActionArea, CardMedia, CardContent } from "@mui/material/"
 import "./Social.css"
@@ -10,24 +10,27 @@ import profiles from "../testing_data/testingData"
 const libraries = ["places"];
 const Profile = ({ profile }) => {
     return (
-        <Card sx={{ maxWidth: 400, position: "absolute", zIndex: "2" }}>
-            <CardActionArea>
-                <CardMedia
-                    component="img"
-                    height="140"
-                    image={profile.image}
-                    alt="profile picture"
-                />
-                <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                        {profile.name}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        {profile.budget}
-                    </Typography>
-                </CardContent>
-            </CardActionArea>
-        </Card>
+        <OverlayView mapPaneName={OVERLAY_MOUSE_TARGET} position={profile.location}>
+            <Card sx={{ maxWidth: 400, position: "absolute", zIndex: "2" }}>
+                <CardActionArea>
+                    <CardMedia
+                        component="img"
+                        height="140"
+                        image={profile.image}
+                        alt="profile picture"
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                            {profile.name}
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            {profile.budget}
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
+            </Card>
+        </OverlayView>
+
     )
 }
 
@@ -48,11 +51,10 @@ const Social = () => {
         setProfile(<Profile profile={profiles[index]} />)
     }
     return (
-        <div>as
+        <div>
             <Navbar />
             <div className="content-container">
                 <PlacesAutocomplete setSelected={setSelected} />
-                {profile ? profile : null}
                 <div className="map-container">
                     <GoogleMap
                         center={center}
@@ -61,8 +63,10 @@ const Social = () => {
                         options={{ styles: mapStyles, streetViewControl: false }}
                         onClick={() => { setProfile(null) }}
                     >
+                        {profile ? profile : null}
                         {selected && <MarkerF position={selected} icon={"http://maps.google.com/mapfiles/ms/icons/blue.png"} />}
                         {profiles.map((profile, index) => { return <MarkerF onClick={(e) => handleProfileClick(e, index)} key={index} position={profile.location} />; })}
+
                     </GoogleMap >
                 </div>
             </div>
