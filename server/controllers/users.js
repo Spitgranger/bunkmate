a07c5e402b3739795ab40f663560c79d7b6b0536
spitgranger <richard.fan2000@hotmail.com>
2023-03-14 20:39:32 -0400

    disabled stream for now

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -5,7 +5,7 @@ import { StreamChat } from "stream-chat";
 import dotenv from 'dotenv'
 import multer from "multer";
 dotenv.config();
-const streamChat = StreamChat.getInstance("asnpsp7e72h6", "djzm2aq63636qg2mjeqx9x5422hb4qu78pqepyf7fx7j7fuu44zwdgathr24zeyu");
+//const streamChat = StreamChat.getInstance("asnpsp7e72h6", "djzm2aq63636qg2mjeqx9x5422hb4qu78pqepyf7fx7j7fuu44zwdgathr24zeyu");
 
 export const signin = async (req, res) => {
     const { email, password } = req.body;
@@ -15,12 +15,14 @@ export const signin = async (req, res) => {
         const isPasswordCorrect = await bcrypt.compare(password, existingUser.password);
         if (!isPasswordCorrect) return res.status(400).json({ message: 'Invalid Credentials' })
         const token = jwt.sign({ email: existingUser.email, id: existingUser._id }, "test", { expiresIn: "1h" });
+        /*
         const { users: [user] } = await streamChat.queryUsers({ id: existingUser._id });
         if (user === null) {
             return res.status(401).send();
         }
         const streamToken = streamChat.createToken(String(existingUser._id));
-        res.status(200).json({ result: existingUser, token, streamToken });
+        */
+        res.status(200).json({ result: existingUser, token /*,streamToken*/ });
     } catch (error) {
         res.status(500).json({ message: 'something went wrong like a gofu' })
         console.log(error)
