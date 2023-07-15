b471ed80599d41f2f7fb1ff83a1eb4bdf556307d
zhwang2001 <zhwang2001@gmail.com>
2023-07-14 21:28:32 -0400

    fixed profilemakerform errors

--- bunkmate-client/src/Components/ProfileMakerForm.tsx
+++ bunkmate-client/src/Components/ProfileMakerForm.tsx
@@ -1,4 +1,4 @@
-import {useState, useRef, useEffect, useContext} from 'react'
+import {JSX, useContext, SyntheticEvent} from 'react'
 import {
     DatePicker,
     FormSection,
@@ -9,49 +9,40 @@ import {
     FormMultiLineInput,
     LineBox,
 } from './Utils/Form.tsx';
-import {IoChevronForward, IoChevronBack} from 'react-icons/io5';
+import {IoChevronForward} from 'react-icons/io5';
 import CameraAltIcon from '@mui/icons-material/CameraAlt';
 import {SignInContext} from './GlobalStateManagement/SignInContext';
-import {UserDataContext} from './GlobalStateManagement/UserDataContext';
-import {useAppDispatch} from "../store/hooks.ts";
-import {setRerender} from "../features/bunkmate/bunkmateSlice.ts";
-import {getProfile} from "../api";
-
-
 import imageCompression from 'browser-image-compression';
 
 //styles
-const backButtonStyles = {
+/*const backButtonStyles = {
     display: 'flex',
     alignItems: 'center',
     padding: '8px'
-}
+}*/
 
 
 //everything below will be displayed within a modal window, this page is shown after signing up for an account
-function ProfileMakerForm({forwardButton, backwardButton}) {
+function ProfileMakerForm(): JSX.Element {
 
-    const {isOpen, setIsOpen} = useContext(SignInContext)
-    const {profileHandleSubmit, profileHandleUpdate} = useContext(UserDataContext)
-    const [userProfile, setUserProfile] = useState("")
-    const reduxDispatch = useAppDispatch()
+    const {setIsOpen} = useContext(SignInContext)
+    //const {profileHandleSubmit, profileHandleUpdate} = useContext(UserDataContext)
+    //const reduxDispatch = useAppDispatch()
 
     //function to handle fetching the profile data from back end
+    /*
     const handleProfile = async () => {
         try {
             return await getProfile();
         } catch (error) {
             console.log(error)
         }
-    };
+    };*/
 
     //get data from backend when the component first loads works
 
-    useEffect(() => {
-        handleProfile().then((profile) => setUserProfile(profile.data))
-    }, []);
 
-
+    /*
     const handleSubmit = (values) => {
         //re-renders the useEffect which fetches info from backend in profile.ts
         //if user already has a profile then update it else submit it
@@ -65,17 +56,20 @@ function ProfileMakerForm({forwardButton, backwardButton}) {
             })
         }
     }
+     */
 
     //special handle event function just to file uploads
-    const handleFileUpload = (e: EventTarget): void => {
-        const file = e.target.files[0];
-        handleConversion(file, (base64String: string): void => {
-            console.log(base64String)
-        }).catch((error) => console.log(error));
+    const handleFileUpload = (e: SyntheticEvent): void => {
+        const fileInput = e.target as HTMLInputElement
+        if (fileInput.files && fileInput.files.length > 0) {
+            handleConversion(fileInput.files[0], (base64String) => {
+                console.log(base64String)
+            }).catch((error) => console.log(error));
+        }
     }
 
     //converts image + compresses to base64-encoded string
-    const handleConversion = async (file: File, callback: (base64String: string) => string): Promise<void> => {
+    const handleConversion = async (file: File, callback: (base64String: ArrayBuffer | string | null) => void): Promise<void> => {
         let compressedFile
         const options = {
             maxSizeMB: 1,
@@ -96,11 +90,12 @@ function ProfileMakerForm({forwardButton, backwardButton}) {
             callback(reader.result);
         };
         reader.onerror = function (error) {
+            console.log("An error has occurred during the file conversion", error)
         };
     }
 
     //handle going back to the previous page
-    const BackButton = () => {
+    /*const BackButton = () => {
         return (
             <label style={{cursor: 'pointer'}}>
                 <input style={{display: 'none'}} onClick={backwardButton} type="button"/>
@@ -108,7 +103,7 @@ function ProfileMakerForm({forwardButton, backwardButton}) {
                     <IoChevronBack/>Back
                 </h3>
             </label>)
-    }
+    }*/
 
     //handle closing the modal window
     const handleClose = () => {
